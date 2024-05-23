003e6f3b6c23bfe8f04f536442fabdb28a06662d
Anish Sharma <sharmaanish023@gmail.com>
2024-05-23 14:05:05 +0545

    Add user and event type defination

---
+++ server/src/types.ts
@@ -0,0 +1,31 @@
+export type GetUserType = {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  created_at: string;
+  updated_at: string;
+};
+
+export type CreateUserType = Omit<
+  GetUserType,
+  "id" | "created_at" | "updated_at"
+>;
+
+export type UpdateUserType = Partial<CreateUserType>;
+
+export type GetEventType = {
+  id: number;
+  title: string;
+  description: string;
+  date: string;
+  created_at: string;
+  updated_at: string;
+};
+
+export type CreateEventType = Omit<
+  GetEventType,
+  "id" | "created_at" | "updated_at"
+>;
+
+export type UpdateEventType = Partial<CreateEventType>;
