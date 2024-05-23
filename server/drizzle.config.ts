9eb7659b47d03a0a075cfae148fe0296b789481c
Anish Sharma <sharmaanish023@gmail.com>
2024-05-23 16:05:43 +0545

    Add drizzle setup and config

---
+++ server/drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  out: "./migrations",
+  dialect: "sqlite",
+  driver: "turso",
+  dbCredentials: {
+    url: process.env.TURSO_CONNECTION_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+  },
+});
