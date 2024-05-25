e38599200c2c0847d589489fa6da9c055ce19bf9
Anish Sharma <sharmaanish023@gmail.com>
2024-05-25 15:58:04 +0545

    Add sidebar component

---
+++ client/src/components/sidebar/sidebar.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/ui/button";
+
+const Sidebar = () => {
+  return (
+    <div className="bg-gray-800 w-64 flex-none rounded-lg">
+      <div className="p-4 text-white">
+        <ul>
+          <li className="p-2 hover:bg-gray-700 cursor-pointer">
+            <Button variant="outline" className="bg-black w-full">
+              Create Event
+            </Button>
+          </li>
+
+          <li className="p-2 hover:bg-gray-700 cursor-pointer">
+            <Button variant="outline" className="bg-black w-full">
+              Join Event
+            </Button>
+          </li>
+
+          <li className="p-2 hover:bg-gray-700 cursor-pointer">
+            <Button variant="outline" className="bg-black w-full">
+              Instant Meet
+            </Button>
+          </li>
+
+          <button className="w-full text-left mt-8">
+            <li className="p-2 hover:bg-gray-700 cursor-pointer text-red-400 text-center">
+              Logout
+            </li>
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
