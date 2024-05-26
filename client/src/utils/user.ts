7f5483ae35a8be0ead149e527350403f02c11e59
Anish Sharma <sharmaanish023@gmail.com>
2024-05-27 00:12:16 +0545

    add user services

---
+++ client/src/utils/user.ts
@@ -0,0 +1,29 @@
+import { api } from "@/lib/api";
+
+export interface CreateUserType {
+  email: string;
+  name: string;
+  password: string;
+}
+
+export type GetUserType = {
+  id: number;
+  name: string;
+  email: string;
+  created_at: string;
+  updated_at: string;
+};
+
+export const createUser = async (user: CreateUserType) => {
+  return await api.post("/user/register", user);
+};
+
+export const loginUser = async (email: string, password: string) => {
+  const response = await api.post("/user/login", { email, password });
+  return response.data;
+};
+
+export const getProfile = async (): Promise<GetUserType> => {
+  const response = await api.get("/user/profile");
+  return await response.data;
+};
