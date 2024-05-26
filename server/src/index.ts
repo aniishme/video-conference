a9fe9e02dff8fbc12d1d4d855b3e805054dc8f23
Anish Sharma <sharmaanish023@gmail.com>
2024-05-27 00:50:28 +0545

    Fix cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,12 @@ import eventRouter from "./routes/event.route";
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
