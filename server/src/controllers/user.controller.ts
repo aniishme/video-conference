32dc8cafd7cbb3a830a39d944e8ad57e2d49eec4
Anish Sharma <sharmaanish023@gmail.com>
2024-05-27 23:01:51 +0545

    FIX getProfile now return user object

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -74,7 +74,7 @@ export class UserController {
       if (!user) {
         return res.status(404).json({ message: "User not found" });
       }
-      return res.status(200).json(user);
+      return res.status(200).json(user[0]);
     } catch (error) {
       next(error);
     }
